4e61258e1dd2ffd30d1640abacd631d4c4c36c99
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-02-21 22:51:08 +0530

    Add files via upload

--- Extractor/__init__.py
+++ Extractor/__init__.py
@@ -1,1 +1,46 @@
+import asyncio
+import logging
+from pyromod import listen
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
+
+
+
+loop = asyncio.get_event_loop()
+
+logging.basicConfig(
+    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
+    level=logging.INFO,
+)
+
+
+
+app = Client(
+    ":Downloader:",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+)
+
+
+
+
+
+
+
+
+async def info_bot():
+    global BOT_ID, BOT_NAME, BOT_USERNAME
+    await app.start()
+    getme = await app.get_me()
+    BOT_ID = getme.id
+    BOT_USERNAME = getme.username
+    if getme.last_name:
+        BOT_NAME = getme.first_name + " " + getme.last_name
+    else:
+        BOT_NAME = getme.first_name
+
+
+loop.run_until_complete(info_bot())
+
 
---
+++ Extractor/__main__.py
@@ -0,0 +1,21 @@
+import asyncio
+import importlib
+from pyrogram import idle
+from Downloader.modules import ALL_MODULES
+
+ 
+
+loop = asyncio.get_event_loop()
+
+
+async def sumit_boot():
+    for all_module in ALL_MODULES:
+        importlib.import_module("Downloader.modules." + all_module)
+
+    print("» ʙᴏᴛ ᴅᴇᴘʟᴏʏ sᴜᴄᴄᴇssғᴜʟʟʏ ✨ 🎉")
+    await idle()
+    print("» ɢᴏᴏᴅ ʙʏᴇ ! sᴛᴏᴘᴘɪɴɢ ʙᴏᴛ.")
+
+
+if __name__ == "__main__":
+    loop.run_until_complete(sumit_boot())
