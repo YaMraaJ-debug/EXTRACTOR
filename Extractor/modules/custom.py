d7caad8b73954458b8964c173a1018133e646cdb
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-02-22 20:30:05 +0530

    Update custom.py

--- Extractor/modules/custom.py
+++ Extractor/modules/custom.py
@@ -1,14 +1,16 @@
 from pyrogram import filters
 from Extractor import app
 from pyromod import listen
-
+from Extractor.core.multi_func import apex_accounts
 
 
 @app.on_message(filters.command(["custom"]) & filters.user(SUDO_USERS))
 async def custom_api(_, message):
   
     editable = await message.reply_text("Send your custom api : like :- https://rksirofficialapi.classx.co.in")
-    await custom_accounts(_, message, apex_api)
+    input1: message = await _.listen(editable.chat.id)
+    apex_api = input1
+    await apex_accounts(_, message, apex_api)
     
 
 
