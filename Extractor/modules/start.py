48f9ce0c4c3209cb292f242451d183b0acf6f11c
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-02-24 16:39:05 +0530

    Update start.py

--- Extractor/modules/start.py
+++ Extractor/modules/start.py
@@ -71,7 +71,7 @@ back_buttons  = [[
 
 @app.on_message(filters.command("start"))
 async def start(_,message):
-  await message.reply_photo(photo="https://telegra.ph/file/9456751a4ca1a346e631f.jpg", 
+  await message.reply_photo(photo="https://graph.org/file/dbd48ba7093582ab20063.jpg", 
                             caption=script.START_TXT.format(message.from_user.mention),
                             reply_markup=button)
 
@@ -90,8 +90,8 @@ async def cb_handler(client, query):
         reply_markup = InlineKeyboardMarkup(buttons)
         try:
             await query.edit_message_text(
-                script.START_TXT.format(query.from_user.mention),
-                reply_markup=reply_markup
+              script.START_TXT.format(query.from_user.mention),
+              reply_markup=reply_markup
             )
         except MessageNotModified:
             pass
