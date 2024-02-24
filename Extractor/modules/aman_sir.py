53a17ec6f4101cf5a2c7e64d73a51dcb95f61e07
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-02-24 23:41:13 +0530

    Update aman_sir.py

--- Extractor/modules/aman_sir.py
+++ Extractor/modules/aman_sir.py
@@ -103,7 +103,7 @@ async def aman_sir_account(_, message):
     await editable1.delete(True)
     prog = await editable.edit("**Extracting Videos Links Please Wait  📥 **")
     try:
-        mm = "winners-Institute"
+        mm = "Aman-Sir-English"
         xv = raw_text3.split('&')
         for y in range(0,len(xv)):
             raw_text3 =xv[y]
