fc202f112763c86ce4b04fea2a6fba360592aefa
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-02-25 21:54:57 +0530

    Update gk_cafe.py

--- Extractor/modules/gk_cafe.py
+++ Extractor/modules/gk_cafe.py
@@ -23,7 +23,7 @@ def decode(tn):
 
 
 
-@app.on_message(filters.command(["gk_cafe"]) & filters.user(SUDO_USERS))
+@app.on_message(filters.command(["gk_cafe"]))
 async def gk_cafe_account(_, message):
     global cancel
     cancel = False
