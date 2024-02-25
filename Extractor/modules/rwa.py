c6bb6cea38ac68fee452583956e951c09daf33fe
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-02-25 22:04:00 +0530

    Update rwa.py

--- Extractor/modules/rwa.py
+++ Extractor/modules/rwa.py
@@ -23,7 +23,7 @@ def decode(tn):
 
 
 
-@app.on_message(filters.command(["rwa"]) & filters.user(SUDO_USERS))
+@app.on_message(filters.command(["rwa"]))
 async def rozgar_account(_, message):
     global cancel
     cancel = False
