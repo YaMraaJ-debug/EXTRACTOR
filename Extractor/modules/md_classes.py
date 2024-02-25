3a50cd1540f3e838bd67ba4fd65c38633e70d3eb
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-02-25 21:56:26 +0530

    Update md_classes.py

--- Extractor/modules/md_classes.py
+++ Extractor/modules/md_classes.py
@@ -23,7 +23,7 @@ def decode(tn):
 
 
 
-@app.on_message(filters.command(["md_classes"]) & filters.user(SUDO_USERS))
+@app.on_message(filters.command(["md_classes"]))
 async def md_classes_account(_, message):
     global cancel
     cancel = False
