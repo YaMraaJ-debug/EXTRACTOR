ce01536aa590dc04934479b9ac3bb58b449c5431
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-02-25 22:02:37 +0530

    Update perfect_academy.py

--- Extractor/modules/perfect_academy.py
+++ Extractor/modules/perfect_academy.py
@@ -23,7 +23,7 @@ def decode(tn):
 
 
 
-@app.on_message(filters.command(["perfect_academy"]) & filters.user(SUDO_USERS))
+@app.on_message(filters.command(["perfect_academy"]))
 async def perfect_academy_account(_, message):
     global cancel
     cancel = False
