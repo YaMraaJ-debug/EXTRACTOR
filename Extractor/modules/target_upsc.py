e63797c598eebe2d0fb376e0140b3c17547c30c5
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-02-25 22:10:00 +0530

    Update target_upsc.py

--- Extractor/modules/target_upsc.py
+++ Extractor/modules/target_upsc.py
@@ -23,7 +23,7 @@ def decode(tn):
 
 
 
-@app.on_message(filters.command(["target_upsc"]) & filters.user(SUDO_USERS))
+@app.on_message(filters.command(["target_upsc"]))
 async def target_upsc_account(_, message):
     global cancel
     cancel = False
