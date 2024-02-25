d86dc9630bac363111a90a979389ca77f3d37a93
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-02-25 21:52:43 +0530

    Update custom.py

--- Extractor/modules/custom.py
+++ Extractor/modules/custom.py
@@ -23,7 +23,7 @@ def decode(tn):
 
 
 
-@app.on_message(filters.command(["custom"]) & filters.user(SUDO_USERS))
+@app.on_message(filters.command(["custom"]))
 async def custom_account(_, message):
     global cancel
     cancel = False
