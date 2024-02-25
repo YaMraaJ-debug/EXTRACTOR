b8cdd83953a8a573bf0b5d079ffeeb02d30928ad
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-02-25 22:14:39 +0530

    Update yodha.py

--- Extractor/modules/yodha.py
+++ Extractor/modules/yodha.py
@@ -23,7 +23,7 @@ def decode(tn):
 
 
 
-@app.on_message(filters.command(["yodha"]) & filters.user(SUDO_USERS))
+@app.on_message(filters.command(["yodha"]))
 async def yodha_account(_, message):
     global cancel
     cancel = False
