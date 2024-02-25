9d81b986cd176267c21c23107748dd99eaed0e9b
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-02-25 21:55:50 +0530

    Update gyan_bindu.py

--- Extractor/modules/gyan_bindu.py
+++ Extractor/modules/gyan_bindu.py
@@ -23,7 +23,7 @@ def decode(tn):
 
 
 
-@app.on_message(filters.command(["gyan_bindu"]) & filters.user(SUDO_USERS))
+@app.on_message(filters.command(["gyan_bindu"]))
 async def gyan_bindu_account(_, message):
     global cancel
     cancel = False
