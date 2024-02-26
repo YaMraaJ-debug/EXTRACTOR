3714060d442aa33e1708ebd628a8e31863d4eeda
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-02-26 10:24:08 +0530

    Update khan.py

--- Extractor/modules/khan.py
+++ Extractor/modules/khan.py
@@ -10,60 +10,60 @@ from base64 import b64decode
 from Extractor import app
 from config import SUDO_USERS
 
-
-
-@app.on_message(filters.command(["khan"]))
+@app.on_message(filters.command(["test"]))
 async def khan_login(_, message):
-    editable = await message.reply_text"Send **ID & Password** in this manner otherwise bot will not respond.\n\nSend like this:-  **ID*Password**")
-    
-    login_url = "https://khanglobalstudies.com/api/login-with-password"  
-    input1: message = await _.listen(editable.chat.id)
+    editable = await message.reply_text("Send ID & Password in this manner otherwise bot will not respond.\n\nSend like this:-  ID*Password")
+
+    login_url = "https://khanglobalstudies.com/api/login-with-password"
+    input1 = await _.listen(editable.chat.id)
     raw_text = input1.text
-    
+
     headers = {
-    "Host": "khanglobalstudies.com",
-    "content-type": "application/x-www-form-urlencoded",
-    "content-length": "36",
-    "accept-encoding": "gzip",
-    "user-agent": "okhttp/3.9.1"
+        "Host": "khanglobalstudies.com",
+        "content-type": "application/x-www-form-urlencoded",
+        "content-length": "36",
+        "accept-encoding": "gzip",
+        "user-agent": "okhttp/3.9.1"
     }
 
     data = {
-    "password": "",
-    "phone": ""
+        "password": "",
+        "phone": ""
     }
     data["username"] = raw_text.split("*")[0]
     data["password"] = raw_text.split("*")[1]
     await input1.delete(True)
-    
+
     response = requests.post(login_url, headers=headers, data=data)
     if response.status_code == 200:
         data = response.json()
         token = data["token"]
-        await editable.edit(f"**Login Successful")
+        await editable.edit("**Login Successful**")
     else:
-         await m.reply_text("Go back to response")
-    
+        await message.reply_text("Go back to response")
+
     headers = {
-    "Host": "khanglobalstudies.com",
-    "authorization": f"Bearer {token}",
-    "accept-encoding": "gzip",
-    "user-agent": "okhttp/3.9.1")
+        "Host": "khanglobalstudies.com",
+        "authorization": f"Bearer {token}",
+        "accept-encoding": "gzip",
+        "user-agent": "okhttp/3.9.1"
     }
+
     course_url = "https://khanglobalstudies.com/api/user/v2/courses"
-    response = requests.get(url, headers=headers)
+    response = requests.get(course_url, headers=headers)
 
     data = response.json()
     courses = [(course['id'], course['title']) for course in data]
-    
-    FFF = "**BATCH-ID  - BATCH-NAME**"
-    
+
+    FFF = "BATCH-ID  - BATCH-NAME\n"
+
     for course_id, course_title in courses:
-        FFF += f"{course_id} - {course_title}"
-    
+        FFF += f"{course_id} - {course_title}\n"
+
     await editable.edit(FFF)
+
+    editable1 = await message.reply_text("Now send the Batch ID to Download")
     
-    editable1= await message.reply_text("**Now send the Batch ID to Download**")
     input3: message = await bot.listen(editable1.chat.id)
     raw_text3 = input3.text
     response2 = s.get(f'https://api.penpencil.xyz/v3/batches/{raw_text3}/details', headers=headers).json()["data"]
