205ffc8cdd55ff8d31cc5052462e992f14ce8443
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-02-27 17:44:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,8 @@ BOT_TOKEN = getenv("BOT_TOKEN", "7128030034:AAFnVgvMp65otGHgEpir9PcbDD2RlTcoQOg"
 OWNER_ID = int(getenv("OWNER_ID", "6280103226"))
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6050277919 6881758615 5783508086 6280103226").split()))
 MONGO_URL = getenv("MONGO_DB", "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority")
-PREMIUM_LOGS = -1002005441647
+
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002054493556"))
+PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1002005441647"))
+
 
