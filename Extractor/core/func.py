759786496460f905c12be5ef9301f72505144e08
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2024-02-27 18:02:25 +0530

    Update func.py

--- Extractor/core/func.py
+++ Extractor/core/func.py
@@ -2,9 +2,13 @@ from config import CHANNEL_ID
 from Extractor.core import script
 
 
+async def gen_link(chat_id):
+   link = await app.export_chat_invite_link(chat_id)
+   return link
 
 async def subscribe(app, message):
    update_channel = CHANNEL_ID
+   url = await gen_link(update_channel)
    if update_channel:
       try:
          user = await app.get_chat_member(update_channel, message.chat.id)
@@ -12,7 +16,7 @@ async def subscribe(app, message):
             await message.reply_text("Sorry Sir, You are Banned. Contact My Support Group @DevsOops")
             return 1
       except UserNotParticipant:
-         await message.reply_photo(photo="https://telegra.ph/file/b7a933f423c153f866699.jpg",caption=script.FORCE_MSG.format(message.from_user.mention), reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🤖 ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ 🤖", url=f"https://t.me/DevsOops")]]))
+         await message.reply_photo(photo="https://telegra.ph/file/b7a933f423c153f866699.jpg",caption=script.FORCE_MSG.format(message.from_user.mention), reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🤖 ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ 🤖", url=f"{url}")]]))
          return 1
       except Exception:
          await message.reply_text("Something Went Wrong. Contact My Support Group")
