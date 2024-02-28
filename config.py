a9234e89da4ae46dbf136476301836fdb67bcb4f
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-02-29 04:30:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,12 @@
 from os import getenv
 
 
-API_ID = int(getenv("API_ID", "26850449"))
-API_HASH = getenv("API_HASH", "72a730c380e68095a8549ad7341b0608")
-BOT_TOKEN = getenv("BOT_TOKEN", "7128030034:AAFnVgvMp65otGHgEpir9PcbDD2RlTcoQOg")
-OWNER_ID = int(getenv("OWNER_ID", "6280103226"))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6050277919 6881758615 5783508086 6280103226").split()))
-MONGO_URL = getenv("MONGO_DB", "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority")
+API_ID = int(getenv("API_ID", "18618422"))
+API_HASH = getenv("API_HASH", "f165b1caec3cfa4df943fe1cbe82d22a")
+BOT_TOKEN = getenv("BOT_TOKEN", "6331627404:AAGa6ICDHJGzgcO0niT8se0ZL0B4dym_UxA")
+OWNER_ID = int(getenv("OWNER_ID", "6050277919"))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6050277919 2112898623").split()))
+MONGO_URL = getenv("MONGO_DB", "mongodb+srv://mohitag24082006:yox8Q1a9P5aLcAzK@cluster0.yafxbo4.mongodb.net/?retryWrites=true&w=majority")
 
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002054493556"))
-PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1002091053529"))
-
-
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002034072106"))
+PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1002034072106"))
