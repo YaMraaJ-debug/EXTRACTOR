28404d496faa2e71a78d8d8ba55a778c9544ff0c
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-03-08 14:51:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+"""
 from os import getenv
 
 
@@ -10,3 +11,33 @@ MONGO_URL = getenv("MONGO_DB", "mongodb+srv://mohitag24082006:yox8Q1a9P5aLcAzK@c
 
 CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002034072106"))
 PREMIUM_LOGS = int(getenv("PREMIUM_LOGS", "-1002034072106"))
+
+"""
+#
+
+
+
+
+# --------------M----------------------------------
+
+import os
+from os import getenv
+# ---------------R---------------------------------
+API_ID = int(os.environ.get("API_ID"))
+# ------------------------------------------------
+API_HASH = os.environ.get("API_HASH")
+# ----------------D--------------------------------
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+# -----------------A-------------------------------
+BOT_USERNAME = os.environ.get("BOT_USERNAME")
+# ------------------X------------------------------
+OWNER_ID = int(os.environ.get("OWNER_ID"))
+# ------------------X------------------------------
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+# ------------------------------------------------
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID"))
+# ------------------------------------------------
+MONGO_URL = os.environ.get("MONGO_URL")
+# -----------------------------------------------
+PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS"))
