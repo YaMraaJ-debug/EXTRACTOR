cc6d04f3f1deb612ce4f2736b0c12b420c551765
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-05-17 10:08:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "18618422"))
 API_HASH = getenv("API_HASH", "f165b1caec3cfa4df943fe1cbe82d22a")
-BOT_TOKEN = getenv("BOT_TOKEN", "6331627404:AAGa6ICDHJGzgcO0niT8se0ZL0B4dym_UxA")
-OWNER_ID = int(getenv("OWNER_ID", "6050277919"))
+BOT_TOKEN = getenv("BOT_TOKEN", "7064501216:AAEOQMaqLGL5e6TBau-8dYhRxUk5yrG3738")
+OWNER_ID = int(getenv("OWNER_ID", "6664582540"))
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6050277919 2112898623").split()))
 MONGO_URL = getenv("MONGO_DB", "mongodb+srv://mohitag24082006:yox8Q1a9P5aLcAzK@cluster0.yafxbo4.mongodb.net/?retryWrites=true&w=majority")
 
