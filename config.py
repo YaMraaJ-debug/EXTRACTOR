992388f582c749219e2241a6896c7c55ae8c602c
PATHAN <137278745+altafpathan65012@users.noreply.github.com>
2024-09-06 09:29:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "23031620"))
 API_HASH = getenv("API_HASH", "31cb00c1cbe580394778b43105864bca")
-BOT_TOKEN = getenv("BOT_TOKEN", "6865731231:AAEDdXn48VOzOf0jRo0CvxkU0VxIFsikTvg")
+BOT_TOKEN = getenv("BOT_TOKEN", "7035220759:AAEQcpQXZOd8aWgJkXQMmUzoeFuU_ufYego")
 OWNER_ID = int(getenv("OWNER_ID", "502980590"))
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "502980590").split()))
 MONGO_URL = mongodb+srv://altafpathan65012:<password>@cluster0.aztmome.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
@@ -27,9 +27,9 @@ API_ID = int(os.environ.get("23031620"))
 # ------------------------------------------------
 API_HASH = os.environ.get("31cb00c1cbe580394778b43105864bca")
 # ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("6865731231:AAEDdXn48VOzOf0jRo0CvxkU0VxIFsikTvg")
+BOT_TOKEN = os.environ.get("7035220759:AAEQcpQXZOd8aWgJkXQMmUzoeFuU_ufYego")
 # -----------------A-------------------------------
-BOT_USERNAME = os.environ.get("pathansavebot")
+BOT_USERNAME = os.environ.get("Verceltxtbot")
 # ------------------X------------------------------
 OWNER_ID = int(os.environ.get("502980590"))
 # ------------------X------------------------------
